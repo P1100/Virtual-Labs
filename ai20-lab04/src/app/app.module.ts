40f41b1fae857e5c338bd333fc54a3104f02ebeb
Pj-3 <51178702+Pj-3@users.noreply.github.com>
2020-05-31 10:44:40 +0200

    Es4 temp

--- ai20-lab04/src/app/app.module.ts
+++ ai20-lab04/src/app/app.module.ts
@@ -3,14 +3,52 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {StudentsComponent} from './students/students.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {NavigationComponent} from './navigation/navigation.component';
+import {TableComponent} from './table/table.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import {DragdropComponent} from './dragdrop/dragdrop.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    NavigationComponent,
+    TableComponent,
+    DragdropComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    LayoutModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTabsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    DragDropModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
