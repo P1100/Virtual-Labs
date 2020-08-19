4f2b3da3850de685d235ac689ba8c75e8264f630
Pj-3 <51178702+Pj-3@users.noreply.github.com>
2020-08-19 20:41:00 +0200

    server: testing JPQL Query con teams ()

--- es2/src/main/java/it/polito/ai/es2/repositories/CourseCustomRepository.java
+++ es2/src/main/java/it/polito/ai/es2/repositories/CourseCustomRepository.java
@@ -1,9 +1,5 @@
 package it.polito.ai.es2.repositories;
 
-import it.polito.ai.es2.entities.Course;
-
-import java.util.List;
-
 public interface CourseCustomRepository {
-  List<Course> advancedSearchCourses();
+  Integer countTeamsThatViolateCardinality(String id, int min, int max);
 }
--- es2/src/main/java/it/polito/ai/es2/repositories/CourseCustomRepositoryImpl.java
+++ es2/src/main/java/it/polito/ai/es2/repositories/CourseCustomRepositoryImpl.java
@@ -1,38 +1,42 @@
 package it.polito.ai.es2.repositories;
 
-import it.polito.ai.es2.entities.Course;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-// TODO: find a way to add this in the final project, add some queries somewhere
 @Repository
 public class CourseCustomRepositoryImpl implements CourseCustomRepository {
   @PersistenceContext
   private EntityManager entityManager;
   
   @Override
-  public List<Course> advancedSearchCourses() {
+  public Integer countTeamsThatViolateCardinality(String id, int min, int max) {
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("id", id);
+    parameters.put("min", min);
+    parameters.put("max", max);
     
     StringBuilder jpql = new StringBuilder();
-    jpql.append("from Course c where c.enabled=true ");
+    jpql.append("select count(*) as countViolatedTeams ");
+    // JPQL, name of entity field (t.course). t.course_id used in nativeQuery
+    jpql.append("from Team t where t.course.id = :id ");
+    jpql.append("and :min > ANY(select count(*) from Team t2 where t2.course.id = :id group by t2.course) ");
+    jpql.append("and :max < ANY(select count(*) from Team t3 where t3.course.id = :id group by t3.course)");
+    TypedQuery<Long> query = entityManager.createQuery(jpql.toString(), Long.class);
+    parameters.forEach(query::setParameter);
     
-    Map<String, Object> parameters = new HashMap<>();
-    
-    jpql.append(" OR c.max > :max ");
-    parameters.put("max", 100);
-    jpql.append(" OR c.id like :id ");
-    parameters.put("id", "_9");
-    
-    TypedQuery<Course> query = entityManager.createQuery(jpql.toString(), Course.class);
-    
-    parameters.forEach(query::setParameter); //parameters.forEach((key, value) -> query.setParameter(key, value));
-    
-    return query.getResultList();
+    Long querySingleResult = null;
+    try {
+      querySingleResult = query.getSingleResult();
+    } catch (NoResultException e) {
+      return 0;
+    }
+    System.out.println("Return Query:" + querySingleResult);
+    return querySingleResult.intValue();
   }
 }
