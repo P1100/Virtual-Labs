dca210a72606da2755b2d301925f7fe527281c2f
Pj-3 <51178702+Pj-3@users.noreply.github.com>
2020-08-25 23:28:48 +0200

    server: csv polish

--- es2/src/main/java/it/polito/ai/es2/domains/StudentViewModel.java
+++ es2/src/main/java/it/polito/ai/es2/domains/StudentViewModel.java
@@ -3,18 +3,26 @@ package it.polito.ai.es2.domains;
 import com.opencsv.bean.CsvBindByName;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Data
 public class StudentViewModel {
   @CsvBindByName
-  @NotBlank
+  @NotNull
   private Long id;
   @CsvBindByName
   @NotBlank
-  private String name;
+  private String firstName;
   @CsvBindByName
   @NotBlank
-  private String firstName;
+  private String lastName;
+  @CsvBindByName
+  @NotBlank
+  @Email
+  @Pattern(regexp = "s[0-9]{1,9}@studenti\\.polito\\.it")
+  private String email;
 }
 
