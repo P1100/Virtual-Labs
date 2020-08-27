fa05633f927492fdae53adc64e00eb76d2137868
Pj-3 <51178702+Pj-3@users.noreply.github.com>
2020-08-27 07:03:04 +0200

    client: form validation for course-edit dialog

--- ai20-lab04/src/app/dialogs/course-edit/course-edit.component.ts
+++ ai20-lab04/src/app/dialogs/course-edit/course-edit.component.ts
@@ -6,7 +6,7 @@ import {CourseService} from '../../services/course.service';
 @Component({
   selector: 'app-course-edit',
   templateUrl: './course-edit.component.html',
-  styleUrls: ['./course-edit.component.css']
+  styles: ['mat-form-field {width: 100%}']
 })
 export class CourseEditComponent {
   course = new Course('null', '', 0, 0, false, '');
@@ -16,21 +16,22 @@ export class CourseEditComponent {
   constructor(private courseService: CourseService,
               public dialogRef: MatDialogRef<CourseEditComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any) {
-    const subscription = courseService.getCourse(data.id).subscribe(c => this.course = c[0]);
+    const subscription = courseService.getCourse(data?.courseId).subscribe(c => this.course = c[0]);
   }
 
   onCancelClick(): void {
-    this.dialogRef.close('cancel');
+    this.dialogRef.close(); // same value as when you press ESC (undefined)
   }
   onSubmit() {
-    this.courseService.updateCourse(this.course).subscribe();
-    this.dialogRef.close('refresh');
+    this.courseService.updateCourse(this.course).subscribe(
+      x => this.dialogRef.close('success'),
+      e => this.dialogRef.close(e?.status)
+    );
   }
 
   public onFileChanged(event) {
     this.course.enabled = !this.course.enabled;
     this.selectedFile = event.target.files[0];
-    console.log(this.selectedFile);
     // To update bootstrap input text, missing JQuery
     this.course.vmModelPath = this.selectedFile.name;
   }
