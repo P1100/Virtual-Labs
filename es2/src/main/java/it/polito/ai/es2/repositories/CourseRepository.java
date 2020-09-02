3448d3e7aaaedfd7d4542e0281b9c37179738101
Pj-3 <51178702+Pj-3@users.noreply.github.com>
2020-09-03 00:46:23 +0200

    server-fix: querying only active teams

--- es2/src/main/java/it/polito/ai/es2/repositories/CourseRepository.java
+++ es2/src/main/java/it/polito/ai/es2/repositories/CourseRepository.java
@@ -10,10 +10,10 @@ import java.util.List;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, String>, CourseCustomRepository {
-  @Query("SELECT DISTINCT s FROM Student s INNER JOIN s.teams t INNER JOIN t.course c WHERE c.id= :courseId")
+  @Query("SELECT DISTINCT s FROM Student s INNER JOIN s.teams t INNER JOIN t.course c WHERE c.id= :courseId AND t.active= true")
   List<Student> getStudentsInTeams(String courseId);
 
-  @Query("SELECT DISTINCT s FROM Student s WHERE s.id NOT IN (SELECT s FROM Student s INNER JOIN s.teams t INNER JOIN t.course c WHERE c.id= :courseId)")
+  @Query("SELECT DISTINCT s FROM Student s WHERE s.id NOT IN (SELECT s FROM Student s INNER JOIN s.teams t INNER JOIN t.course c WHERE c.id= :courseId AND t.active= true)")
   List<Student> getStudentsNotInTeams(String courseId);
 
   // CustomRepository: Integer countTeamsThatViolateCardinality(String courseId, int min, int max);
