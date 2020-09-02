456a9963698753bb767e414b518a6086affc5e3e
Pj-3 <51178702+Pj-3@users.noreply.github.com>
2020-09-02 16:26:23 +0200

    client: checkbox logic for teams available table

--- ai20-lab04/src/app/r2-inner-tab/student/teams/teams-cont.component.ts
+++ ai20-lab04/src/app/r2-inner-tab/student/teams/teams-cont.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnDestroy} from '@angular/core';
 import {Student} from '../../../models/student.model';
-import {Subscription} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import {ActivatedRoute} from '@angular/router';
 import {AlertsService} from '../../../services/alerts.service';
 import {CourseService} from '../../../services/course.service';
@@ -8,12 +8,12 @@ import {CourseService} from '../../../services/course.service';
 @Component({
   selector: 'app-teams-cont',
   template: `
-    <app-teams [enrolledWithoutTeams]="enrolledWithoutTeams">
+    <app-teams [enrolledWithoutTeams]="enrolledWithoutTeams"
+               (forceUploadData)="onForceUploadData($event)">
       <!--                  [students]="allStudents"-->
       <!--                  (enrolledEvent)="onStudentsToEnroll($event)"-->
       <!--                  (disenrolledEvent)="onStudentsToDisenroll($event)"-->
       <!--                  (uploadCsvEvent)="onCsvUpload($event)"-->
-      >
     </app-teams>
   `,
   styleUrls: []
@@ -21,23 +21,32 @@ import {CourseService} from '../../../services/course.service';
 export class TeamsContComponent implements OnDestroy {
   enrolledWithoutTeams: Student[] = [];
   courseId = '0';
-  subEnrolledStudentsCourse: Subscription = null;
+  private observableEnrolledWithoutTeam: Observable<Student[]>;
+  subEnrolledWithTeams: Subscription = null;
   subRouteParam: Subscription = null;
 
   constructor(private courseService: CourseService, private activatedRoute: ActivatedRoute, private alertsService: AlertsService) {
     this.subRouteParam = this.activatedRoute.paramMap.subscribe(() => {
         this.courseId = this.activatedRoute.parent.snapshot.paramMap.get('id');
-        this.subEnrolledStudentsCourse = this.courseService.getEnrolledWithoutTeam(this.courseId)
+        this.observableEnrolledWithoutTeam = this.courseService.getEnrolledWithoutTeam(this.courseId); // reusable only for the same course id
+        this.subEnrolledWithTeams = this.observableEnrolledWithoutTeam
           .subscribe((students: Student[]) => {
-              console.log('AAAAAAAAA', students);
+              console.log('observableEnrolledWithoutTeam init', students);
               this.enrolledWithoutTeams = Array.isArray(students) ? [...students] : [];
-            }, error => this.alertsService.setAlert('danger', 'Couldn\t get enrolled students! ' + error)
+            }, error => this.alertsService.setAlert('danger', 'Couldn\'t get enrolled without team! ' + error)
           );
       }
     );
   }
   ngOnDestroy(): void {
     this.subRouteParam.unsubscribe();
-    this.subEnrolledStudentsCourse.unsubscribe();
+    this.subEnrolledWithTeams.unsubscribe();
+  }
+  onForceUploadData($event: any) {
+    this.observableEnrolledWithoutTeam.subscribe((students: Student[]) => {
+        console.log('observableEnrolledWithoutTeam update', students);
+        this.enrolledWithoutTeams = Array.isArray(students) ? [...students] : [];
+      }, error => this.alertsService.setAlert('danger', 'Couldn\'t get enrolled without team! ' + error)
+    );
   }
 }
--- ai20-lab04/src/app/r2-inner-tab/student/teams/teams.component.ts
+++ ai20-lab04/src/app/r2-inner-tab/student/teams/teams.component.ts
@@ -1,8 +1,12 @@
-import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
 import {Student} from '../../../models/student.model';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatSort} from '@angular/material/sort';
 import {animate, state, style, transition, trigger} from '@angular/animations';
+import {CourseAddComponent} from '../../../dialogs/course-add/course-add.component';
+import {AlertsService} from '../../../services/alerts.service';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import {CourseService} from '../../../services/course.service';
 
 @Component({
   selector: 'app-teams',
@@ -16,26 +20,71 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
     ]),
   ],
 })
-export class TeamsComponent implements OnInit {
+export class TeamsComponent implements OnInit, OnDestroy {
   dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>();
+  displayedColumns: string[] = ['select', 'id', 'firstName', 'lastName', 'email'];
+  private enrolledSelectedForProposal: Student[];
+  dialogRef: MatDialogRef<any>;
+  /* Checkbox Logic */
+  checkboxMasterCompleted = false;
+  checkboxMasterIndeterminate = false;
+  checked: Map<number, boolean> = new Map();
+
   @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  @Output()
+  forceUploadData = new EventEmitter<any>();
+
   @Input()
   set enrolledWithoutTeams(array: Student[]) {
     this.dataSource.data = [...array];
-    // this.checked = new Map(array.map(x => [+x.id, false]));
+    this.checked = new Map(array.map(x => [+x.id, false]));
   }
   get enrolledWithoutTeams(): Student[] {
     return this.dataSource.data;
   }
-  displayedColumns: string[] = ['select', 'id', 'firstName', 'lastName', 'email'];
 
-  constructor() {
+  constructor(private alertsService: AlertsService, private courseService: CourseService, public dialog: MatDialog,) {
   }
-
   ngOnInit() {
     this.dataSource.sort = this.sort;
   }
 
   openProposeTeamDialog() {
+    if (this.dialogRef) {
+      return;
+    }
+    this.dialogRef = this.dialog.open(CourseAddComponent, {
+      maxWidth: '400px', autoFocus: true, hasBackdrop: true, disableClose: true, closeOnNavigation: true,
+      data: {studentsProposal: this.enrolledSelectedForProposal}
+    });
+    this.dialogRef.afterClosed().subscribe((res: string) => {
+        this.dialogRef = null;
+        if (res != undefined) {
+          this.forceUploadData.emit(null);
+        }
+      }, () => this.alertsService.setAlert('danger', 'Team Proposal dialog error')
+    );
+  }
+  // change selection, sort update, paginator update
+  private updateMasterCheckbox() {
+    const entriesCheckbox = [...this.checked.entries()];
+    this.checkboxMasterCompleted = entriesCheckbox.every(t => t[1] === true);
+    this.checkboxMasterIndeterminate = !this.checkboxMasterCompleted && entriesCheckbox.filter(x => x[1] === true).length > 0;
+  }
+  checkboxChangeSelection({checked}: { checked: boolean }, id) { // destructuring
+    this.checked.set(+id, checked);
+    this.updateMasterCheckbox();
+  }
+  checkboxIsChecked(id: number) {
+    return this.checked.get(+id);
+  }
+  checkboxSetAll(completed: boolean) {
+    [...this.checked.entries()].forEach((t: [number, boolean]) => this.checked.set(+t[0], completed)); // converting iterable to array (spread)
+    this.checkboxMasterCompleted = completed;
+    this.checkboxMasterIndeterminate = false;
+  }
+  ngOnDestroy(): void {
+    this.dialogRef.close();
   }
 }
