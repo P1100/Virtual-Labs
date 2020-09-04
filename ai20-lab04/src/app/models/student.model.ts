343017daddbb96331db5ec3b01c892af84d595a1
Pj-3 <51178702+Pj-3@users.noreply.github.com>
2020-09-04 23:19:38 +0200

    client: removed unused teamName field

--- ai20-lab04/src/app/models/student.model.ts
+++ ai20-lab04/src/app/models/student.model.ts
@@ -4,21 +4,11 @@ export class Student {
   firstName: string;
   lastName: string;
   email: string;
-  teamName?: string;
 
   constructor(id: number, firstName: string, lastName: string, email: string) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
-  }
-
-  toString(): string {
-    return this.lastName + ' ' +
-      this.firstName + ' (' +
-      this.id + ')' + ' (' +
-      this.teamName + ')' + ' (' +
-      this.email + ')'
-      ;
   }
 }
