2f2d79e58446c3ca797b98d04d05a4ce99e2dd37
Pj-3 <51178702+Pj-3@users.noreply.github.com>
2020-09-06 18:26:06 +0200

    server: scheduled teams cleaning task

---
+++ es2/src/main/java/it/polito/ai/es2/ScheduledTasks.java
@@ -0,0 +1,17 @@
+package it.polito.ai.es2;
+
+import it.polito.ai.es2.services.interfaces.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+  @Autowired
+  private TeamService teamService;
+
+  @Scheduled(cron = "0 0 0 25 12 ?")
+  public void reportCurrentTime() {
+    teamService.cleanupTeamsExpiredDisabled();
+  }
+}
