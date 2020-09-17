b1dad8e83d39dbd14ee294a3457fe1f2f4af7805
Pj-3 <51178702+Pj-3@users.noreply.github.com>
2020-09-17 19:34:28 +0200

    restored dev settings

--- es2/src/main/java/it/polito/ai/es2/securityconfig/jwt/JwtAuthenticationEntryPoint.java
+++ es2/src/main/java/it/polito/ai/es2/securityconfig/jwt/JwtAuthenticationEntryPoint.java
@@ -17,6 +17,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Se
   public void commence(HttpServletRequest request, HttpServletResponse response,
                        AuthenticationException authException) throws IOException {
     System.out.println(authException);
-    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "JWT: Unauthorized");
+    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
   }
 }
